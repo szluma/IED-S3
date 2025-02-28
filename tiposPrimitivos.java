aa60d64c7fb3844e84410bd8f98ec70359a694ee
luma <lumadesouzaferreira1@gmail.com>
2025-02-28 10:14:41 -0300

    Add files via upload

---
+++ tiposPrimitivos.java
@@ -0,0 +1,18 @@
+public class tiposPrimitivos {
+
+    public static void main(String args[]){
+        boolean flag = true;
+        char ch = 'A';
+        byte b = 12;
+        short s = 24;
+        int i = -123;
+        long l = 890L;
+
+        System.out.println("Variavel booleana = " + flag);
+        System.out.println("Variavel Char = " + ch);
+        System.out.println("Variavel byte = " + b);
+        System.out.println("Variavel Short = " + s);
+        System.out.println("Variavel Int = " + i);
+        System.out.println("Variavel Long = " + l);
+    }
+}
